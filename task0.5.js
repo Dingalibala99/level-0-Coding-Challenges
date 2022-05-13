db12bc486757c5afee9a71619ea639de9fd86e26
Dingalibala99 <102686868+Dingalibala99@users.noreply.github.com>
2022-05-13 13:29:15 +0200

    Update task0.5.js

--- task0.5.js
+++ task0.5.js
@@ -1,6 +1,6 @@
 function triangleArea(length1,length2,length3) {
-    let semiParimeter = (length1 + length2 + length3)/2
-    let area = Math.sqrt(semiParimeter*(semiParimeter - length1)*(semiParimeter - length2)*(semiParimeter - length3))
+    const semiParimeter = (length1 + length2 + length3)/2
+    const area = Math.sqrt(semiParimeter*(semiParimeter - length1)*(semiParimeter - length2)*(semiParimeter - length3))
     return area;
 }
 console.log(triangleArea(4,5,3));

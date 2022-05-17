b41d95050c5f9124ad82b7033b50d8c33ddfe91c
Dingalibala99 <102686868+Dingalibala99@users.noreply.github.com>
2022-05-17 18:13:39 +0200

    Update task0.6.js

--- task0.6.js
+++ task0.6.js
@@ -1,12 +1,12 @@
-
 function getMaximum(val1, val2, val3) {
-    if (val1 > val2 && val1 > val3) {
+    if (val1 >= val2 && val1 >= val3) {
       maximumValue = val1;
-    } else if (val2 > val1 && val2>val3) {
+    } else if (val2 >= val1 && val2>= val3) {
       maximumValue = val2;
-    } else if (val3 > 1 && val3 > val2) {
+    } else if (val3 >= val1 && val3 >= val2) {
       maximumValue = val3;
-    }
+    }   
     return maximumValue;
-  }
-  console.log(getMaximum(100,50,10));
+    }
+  console.log(getMaximum(50,60,60));
+
